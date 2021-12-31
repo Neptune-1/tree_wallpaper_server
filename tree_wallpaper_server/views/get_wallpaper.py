c40102f00e8f94a6d2cafcd51766be0f888db9c2
alex <phobos2019@gmail.com>
2021-12-31 14:41:32 +0100

    upgrade for trees' generation

--- tree_wallpaper_server/views/get_wallpaper.py
+++ tree_wallpaper_server/views/get_wallpaper.py
@@ -12,8 +12,8 @@ def get_wallpaper(request):
     screen_size_y = request.params.get('screen_size_y')
     inverse = request.params.get('inverse')
     if screen_size_x is None:
-        screen_size_x = 1920
-        screen_size_y = 1080
+        screen_size_x = 1920 * 2
+        screen_size_y = 1080 * 2
     if inverse is None:
         inverse = False
     image_file = GetFractalTreeWallpaper((screen_size_x, screen_size_y), inverse).get_wallpaper()
@@ -25,25 +25,49 @@ def get_wallpaper(request):
 
 class GetFractalTreeWallpaper:
 
-    def __init__(self, screen_size, inverse):
+    def __init__(self, screen_size, inverse):  # screen_size - tuple (x, y) , inverse - bool
+        self.screen_size = screen_size  # (x, y)
+
         self.inverse = inverse
         self.screen_size = screen_size[0] * 4, screen_size[1] * 4
-
-        self.im = Image.new('RGB', self.screen_size, (lambda: (20, 20, 20) if self.inverse else (245, 245, 245))())
-        self.draw = ImageDraw.Draw(self.im)
-        self.null_alpha = pi / 2 + (random() - 0.5) / 5
+        self.im = Image.new('RGB', screen_size, (lambda: (20, 20, 20) if self.inverse else (245, 245, 245))())
+        self.draw = ImageDraw.Draw(self.im)  # draw object to draw on image
+        self.null_alpha = pi / 2 + (random() - 0.5) / 5  # angle of main branch
         self.base_alpha = pi / randint(5, 10)
-        self.null_l = self.screen_size[1] / randint(6, 8)
-        self.null_point = (self.screen_size[0] / 2, self.screen_size[1] / 5 * 4)
-        self.full_n = 0
+        self.null_l = screen_size[1] / randint(6, 8)  # length of main branch
+        self.null_point = (screen_size[0] / 2, screen_size[1] / 5 * 4) # begin point of main branch
+        self.full_n = 0 # initial number of branches
 
-        self.wind_influence = random() / 5 + 0.9  # >1 main rotation in left side, <1 ... in right side
-        for i in range(5):
-            self.draw.line((0, self.screen_size[1] / 5 * 3.5 + (random() - 0.5) * 7, self.screen_size[0],
-                            self.screen_size[1] / 5 * 3.5 - (random() - 0.5) * 7), width=1,
-                           fill=(lambda: (220, 220, 220) if self.inverse else (50, 50, 50))())
+        self.wind_influence = random() / 3 + 0.84  # >1 main rotation in left side, <1 ... in right side
 
-    def branch(self, point, alpha, n):
+        self.start_width = self.screen_size[0] * 0.004  # width of main branch
+
+    def draw_branch(self, begin, end, alpha, width1, width2, n,
+                    alpha_pre):  # begin - tuple (x, y), end - tuple (x, y), alpha - angle, width - int
+
+        angle = pi / 2 - (self.null_alpha * self.wind_influence ** n + alpha)
+        angle_pre = pi / 2 - (self.null_alpha * self.wind_influence ** (n - 1) + alpha_pre)
+        if n == 1:
+            angle_pre = 0
+        p1 = (begin[0] - cos(angle_pre) * (width1 / 2), begin[1] - sin(angle_pre) * (width1 / 2))
+        p2 = (begin[0] + cos(angle_pre) * (width1 / 2), begin[1] + sin(angle_pre) * (width1 / 2))
+        p3 = (end[0] + cos(angle) * (width2 / 2), end[1] + sin(angle) * (width2 / 2))
+        p4 = (end[0] - cos(angle) * (width2 / 2), end[1] - sin(angle) * (width2 / 2))
+        self.draw.polygon([p1, p4, p3, p2],
+                          fill=self.get_color(n))
+
+    def get_width(self, n):  # n - number of iteration
+        return round(self.start_width * 0.8 ** (n * 1.9))
+
+    def get_color(self, n):  # n - number of iteration
+        if self.inverse:
+            return 220, 220, 220, 255 - n * 2
+        else:
+            return 0, 0, 0, 100 + n * 10
+
+    # point - tuple (x, y), alpha - angle, n - number of iteration, alpha_pre - angle of previous branch
+    def branch(self, point, alpha, n, alpha_pre):
+        alpha = alpha % (pi * 2)
         self.full_n += 1
         x, y = point
         length_coef = random() / 10 + 0.7 + 0.1 ** n
@@ -53,28 +77,27 @@ class GetFractalTreeWallpaper:
 
         n += 1
 
-        self.draw.line((x, y, x1, y1), width=round(self.screen_size[0] * 0.01 * 0.8 ** (n * 1.5)),
-                       fill=(lambda: (220, 220, 220) if self.inverse else (n * 3, n * 3, n * 3))())
+        self.draw_branch((x, y), (x1, y1), alpha, self.get_width(n), self.get_width(n + 1), n, alpha_pre)
 
         if n < 15:
             for i in range(randint(1, 2)):
                 r = random()
-                if r < 0.5:
-                    self.branch((x1, y1), alpha - i * self.base_alpha * (random() / 5 + 1), n)
-                    self.branch((x1, y1), alpha + i * self.base_alpha * (random() / 5 + 1), n)
-                elif r < 0.51:
-                    self.branch((x1, y1), alpha + i * self.base_alpha * (random() / 5 + 1), n)
-                elif r < 0.9:
-                    self.branch((x1, y1), alpha - i * self.base_alpha * (random() / 5 + 1), n)
+                if r < 0.5:  # probability of 2 branches
+                    self.branch((x1, y1), alpha - i * self.base_alpha * (random() / 5 + 1), n, alpha)
+                    self.branch((x1, y1), alpha + i * self.base_alpha * (random() / 5 + 1), n, alpha)
+                elif r < 0.51:  # probability of right branch
+                    self.branch((x1, y1), alpha + i * self.base_alpha * (random() / 5 + 1), n, alpha)
+                elif r < 0.9:  # probability of left branch
+                    self.branch((x1, y1), alpha - i * self.base_alpha * (random() / 5 + 1), n, alpha)
 
     def get_wallpaper(self):
-        while self.full_n < 50000:
+        while self.full_n < 40000:
             print("start")
-            self.branch(self.null_point, 0, 0)
-            print(self.full_n)
+            self.branch(self.null_point, 0, 0, 0)
+            print(self.full_n)  # print number of branches
 
-        self.im = self.im.resize((self.screen_size[0] // 2, self.screen_size[1] // 2), resample=Image.ANTIALIAS)
-
+        self.im = self.im.resize((self.screen_size[0] // 2, self.screen_size[1] // 2),
+                                 resample=Image.ANTIALIAS)  # smooth image
         temp = BytesIO()
         self.im.save(temp, format="png")
 
